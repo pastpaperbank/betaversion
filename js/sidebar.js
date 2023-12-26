1ee48a06844e7c4b5932f9d43adff393ebeea7e9
PAST PAPER BANK <pastpaperbank2@gmail.com>
2023-12-26 11:19:32 +0630

    Past Paper Bank Beta Version

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var clicker = document.querySelector('.second_sidebar_clicker');
+    var clickers = document.querySelectorAll('.second_sidebar_clicker, .hamburger_menu');
 
-    clicker.addEventListener('click', function () {
-        clicker.classList.toggle('clicked');
+    clickers.forEach(function (clicker) {
+        clicker.addEventListener('click', function () {
+            // Toggle the "clicked" class for the clicked element
+            clicker.classList.toggle('clicked');
+            
+            // Close other dropdowns when one is clicked
+            clickers.forEach(function (otherClicker) {
+                if (otherClicker !== clicker) {
+                    otherClicker.classList.remove('clicked');
+                }
+            });
+        });
     });
 });
